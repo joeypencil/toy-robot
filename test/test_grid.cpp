7878a9751f4e07bbb04702b76c66cbd5d732baf8
Stephen Joe Tiamsing <scruz@openit.com>
2022-09-13 19:57:55 +0800

    [TEST] Add unit test for Grid class

---
+++ test/test_grid.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include <memory>
+#include "coordinates.h"
+#include "grid.h"
+
+using namespace ToyRobot;
+
+
+namespace ToyRobotUnitTest
+{
+    TEST( GridTest, CanCheckLocations )
+    {
+        Coordinates dimensions( 4, 4 );
+        auto grid = std::make_shared<Grid>( dimensions );
+
+        Coordinates location_valid( 1, 3 );
+        EXPECT_TRUE( grid->IsWithinGrid( location_valid ) );
+
+        Coordinates location_invalid( 5, 5 );
+        EXPECT_FALSE( grid->IsWithinGrid( location_invalid ) );
+    }
+
+    TEST( GridTest, WillThrowOnInvalidDimensions )
+    {
+        Coordinates dimension_zero( 0, 4 );
+        EXPECT_THROW( Grid grid( dimension_zero ), const char* );
+
+        Coordinates dimension_negative( 3, -1 );
+        EXPECT_THROW( Grid grid( dimension_negative ), const char* );
+    }
+}
